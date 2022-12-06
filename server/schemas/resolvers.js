60331ff5eff63cee34aaea4e8473836fd3178615
jesus-orduno <jesus.orduno@gmail.com>
2022-12-06 02:57:09 -0700

    cleaned up code causing errors

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,20 +5,6 @@ const { signToken } = require('./auth');
 
 const resolvers = {
   Query: {
-    getUser: async ({ _id }) => {
-      try {
-        const foundUser = await User.findOne({ _id });
-
-        if (!foundUser) {
-          return res.status(400).json({ message: 'Cannot find a user with this id!' });
-        }
-
-        return foundUser;
-      } catch (err) {
-        console.log(error);
-      }
-    },
-
     me: async (parent, args, context) => {
       if (context.user) {
         try {
@@ -37,17 +23,6 @@ const resolvers = {
   },
 
   Mutation: {
-    createUser: async (parent, { username, email, password }) => {
-      try {
-        console.log(email, username, password);
-        const user = await User.create({ username, email, password });
-        const token = signToken(user);
-        return { token, user };
-      } catch (err) {
-        console.log(err);
-      }
-    },
-
     login: async (parent, { email, password }) => {
       try {
         const user = await User.findOne({ email });
@@ -69,21 +44,6 @@ const resolvers = {
         )
 
         return updateUserBooks;
-      } catch (err) {
-        console.log(err);
-      }
-    },
-
-    deleteBook: async (parent, { bookId }, context) => {
-      try {
-        const deletedBook = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId: bookId } } },
-          { new: true }
-        );
-
-        return deletedBook;
-
       } catch (err) {
         console.log(err);
       }
